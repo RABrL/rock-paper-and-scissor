2bbddea03aaacb3fb29b7ceec648feabca7572a9
Andres Brito <robin_brito@hotmail.com>
2022-09-20 17:34:52 -0500

    First commit

--- main.js
+++ main.js
@@ -1,44 +1,68 @@
-let intentos = Math.floor(parseInt(prompt('cuantos veces quiere jugar')))
 let puntosJugador=0,puntosMaquina=0
-while(intentos>0){
-    let opcionJugador = parseInt(prompt('Elige una opcion:\n\n1.Piedra\n2.Papel\n3.Tijera'))
-    if(opcionJugador>3 || opcionJugador<1){
-        alert('opcion invalida, Perdiste')
+let ataqueEnemigo,ataqueJugador,intentos
+const ataques = ['Piedra','Papel','Tijera']
+const empezar = document.querySelector('.start')
+empezar.addEventListener('click',elegirIntentos)
+
+function elegirIntentos(){
+    intentos = Math.floor(parseInt(prompt('cuantos veces quiere jugar?')))
+    if(intentos<1 || !intentos){
+        alert('Numeros mayores que 0 porfavor')
+        elegirIntentos()
+    }else{
+        elegirAtaqueJugador()
+    }
+}
+function elegirAtaqueJugador(){
+    ataqueJugador = parseInt(prompt('Elige una opcion:\n\n1.Piedra\n2.Papel\n3.Tijera'))
+    if(ataqueJugador<1 || !ataqueJugador || ataqueJugador>3){
+        alert('Elija un ataque valido!')
+        elegirAtaqueJugador()
+    }else{
+        ataqueJugador=ataques[ataqueJugador-1]
+        elegirAtaqueMaquina()
+    }
+}
+function elegirAtaqueMaquina(){
+    let numRandom = numAleatorio(0,ataques.length-1)
+    ataqueEnemigo=ataques[numRandom]
+    pelea()
+}
+function pelea(){
+    if(ataqueEnemigo===ataqueJugador){
+        alert('Es un empate ambos jugador sacaron ' + ataqueEnemigo)
+    }else if(ataqueEnemigo=='Papel' && ataqueJugador=='Piedra'){
+        alert('Tu sacaste ' + ataqueJugador + ' y la maquina saco ' + ataqueEnemigo + '\nPerdiste')
+        puntosMaquina++
+    }else if(ataqueEnemigo=='Tijera' && ataqueJugador=='Papel'){
+        alert('Tu sacaste ' + ataqueJugador + ' y la maquina saco ' + ataqueEnemigo + '\nPerdiste')
+        puntosMaquina++
+    }else if(ataqueEnemigo=='Piedra' && ataqueJugador=='Tijera'){
+        alert('Tu sacaste ' + ataqueJugador + ' y la maquina saco ' + ataqueEnemigo + '\nPerdiste')
         puntosMaquina++
     }else{
-        let opcionMaquina = Math.floor(Math.random()*3+1)
-        if(opcionJugador===opcionMaquina){
-            alert('Es un empate ambos jugador sacaron')
-        }else if (opcionJugador===1 && opcionMaquina===2){
-            alert('Tu sacaste Piedra y la maquina saco papel\nPerdiste')
-            puntosMaquina++
-        }else if(opcionJugador===2 && opcionMaquina===3){
-            alert('Tu sacaste Papel y la maquina saco Tijera\nPerdiste')
-            puntosMaquina++
-        }else if(opcionJugador===3 && opcionMaquina===1){
-            alert('Tu sacaste Tijera y la maquina saco Piedra\nPerdiste')
-            puntosMaquina++
-        }else{
-            if(opcionJugador===1 && opcionMaquina===3){
-                alert('Tu sacaste Piedra y la maquina saco Tijera\nGanaste')
-                puntosJugador++
-            }else if(opcionJugador===2 && opcionMaquina===1){
-                alert('Tu sacaste Papel y la maquina saco Piedra\nGanaste')
-                puntosJugador++
-            }else if(opcionJugador===3 && opcionMaquina===2){
-                alert('Tu sacaste Tijera y la maquina saco Papel\nGanaste')
-                puntosJugador++
-            }  
-        }
+        alert('Tu sacaste ' + ataqueJugador + ' y la maquina saco ' + ataqueEnemigo + '\nGanaste')
+        puntosJugador++
     }
-    if(intentos===1){
+    revisarIntentos()
+}
+function revisarIntentos(){
+    intentos--
+    if(intentos<=0){
+        resultadoBatalla()
+    }else{
+        elegirAtaqueJugador()
+    }
+}
+function resultadoBatalla(){
     if(puntosJugador==puntosMaquina){
         alert('Que curioso, han quedado empate')
     }else if (puntosJugador>puntosMaquina){
-        alert('Felicitaciones, has ganado con un puntaje de ' + puntosJugador + ' vs un puntaje de ' + puntosMaquina + ' de la maquna')
+        alert('Felicitaciones, has ganado con un puntaje de ' + puntosJugador + ' vs un puntaje de ' + puntosMaquina + ' de la maquina')
     }else{
-        alert('Que triste :(, has perdido con un puntaje de ' + puntosJugador + ' vs un puntaje de ' + puntosMaquina + ' de la maquna')
+        alert('Que triste :(, has perdido con un puntaje de ' + puntosJugador + ' vs un puntaje de ' + puntosMaquina + ' de la maquina')
     }
-    }
-    intentos--
+}
+function numAleatorio(min,max){
+    return Math.floor(Math.random()*(max-min+1))
 }
